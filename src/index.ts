e71e0eb8895339e7b102ca6e7715f9832c931354
Alexey Belgradski <Alexeybelgradski@yandex.ru>
2025-08-07 10:59:43 +0300

    create page.ts and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 import './scss/styles.scss';
+import { API_URL, CDN_URL } from './utils/constants';
+import { StateApp } from './components/StateApp';
+import { EventEmitter } from './components/base/events';
+import { LarekApi } from './components/larekApi';
+import { Page } from './components/Page';
+
+
+
+
+
+const evt = new EventEmitter();
+const api = new LarekApi(CDN_URL, API_URL);
+const stateData = new StateApp({}, evt);
+const page = new Page(document.body, evt)
